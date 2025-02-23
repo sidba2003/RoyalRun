981b936d07a3d41c911b644bb2f5bf3b188d9dea
sidme <bahlsiddharth123@gmail.com>
2025-02-23 03:03:51 +0000

    Adding support for collisions

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(other.gameObject.name);
+        }
+    }
+}
