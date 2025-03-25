7e1bac67f10d0b1601d2fd88ef184a4f2054af17
sidme <bahlsiddharth123@gmail.com>
2025-03-25 21:12:13 +0000

    adding fade out effect and game over screen

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    [Tooltip("Text area for the time manager UI")]
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] float startTimeLeft;
+    [SerializeField] Image fadeOutEffect;
+    [SerializeField] GameObject gameOver;
+    [SerializeField] float fadeOutEffectDuration;
+
+    float timeLeft;
+    bool fadeCalled = false;
+
+    private void Start()
+    {
+        timeLeft = startTimeLeft;
+        text.text = timeLeft.ToString("F1");
+    }
+
+    private void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft >= 0) text.text = timeLeft.ToString("F1");
+
+        if (timeLeft <= 0)
+        {
+            Time.timeScale = 0.1f;
+            gameOver.SetActive(true);
+
+
+            if (!fadeCalled)
+            {
+                StartCoroutine(FadeOutEffect());
+                fadeCalled = true;
+            }
+        }
+    }
+
+    IEnumerator FadeOutEffect()
+    {
+        Image fadeImage = fadeOutEffect.GetComponent<Image>();
+        Color imC = fadeImage.color;
+
+        float time = 0;
+
+        while (time <= fadeOutEffectDuration)
+        {
+            time += (Time.deltaTime * 10);
+            float aVal = Mathf.Lerp(0, 1, time / fadeOutEffectDuration);
+
+            imC.a = aVal;
+            fadeImage.color = imC;
+
+            yield return null;
+        }
+
+        imC.a = 1;
+        fadeImage.color = imC;
+
+        Application.Quit();
+    }
+}
